743b206c204286c5262fb1d0f4b5f3b3cd91e008
Vitor <vytroxalberto@gmail.com>
2023-12-20 19:35:25 -0300

    Todo finalizado

--- src/test/java/br/com/vitoralberto/desafiotodolist/DesafioTodolistApplicationTests.java
+++ src/test/java/br/com/vitoralberto/desafiotodolist/DesafioTodolistApplicationTests.java
@@ -3,11 +3,13 @@ package br.com.vitoralberto.desafiotodolist;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class DesafioTodolistApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
